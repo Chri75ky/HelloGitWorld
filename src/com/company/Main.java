c0255cf212593553ee5f952b3eecf89d7b6f57b2
chri75ky <chri75ky@stud.kea.dk>
2021-09-27 09:52:59 +0200

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Git World!");
+
+        System.out.println("Hello again, Git World!");
     }
 }
